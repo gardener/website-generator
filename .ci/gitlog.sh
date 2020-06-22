69c592f8c3a979dad95edd07d02a66f081349737
Georgi Pavlov <georgi.pavlov@sap.com>
2020-06-22 20:45:37 +0300

    merging commit info with github author info

--- .ci/gitlog.sh
+++ .ci/gitlog.sh
@@ -5,33 +5,18 @@ set -e
 # the path relative to `content-source-path`.
 # The `content-folder-path` directory is expected to be in a git repo clone.
 #
-# Usage: gitlog.sh content-folder-path(website-generator/hugo/content) content-source-path(documentation/website) output-folder-path(website-generator/hugo/data)
+# Usage: gitlog.sh content-folder-path(website-generator/hugo/content) file-path(documentation/website/documentation/_index.md)
 # All paths are absolute
 function gitlog {
-for _f in "$1"/*
-do
-  f=$(readlink -f "$_f")
-  source=$(readlink -f "$2")
-  data=$(readlink -f "$3")
-  if [[ -d $f ]]
-  then
-    cd $f
-    gitlog $f $source $data
-    cd ..
-  else
-    fileext=${f##*.}
-    if [[ "$fileext" != "md" && "$fileext" != "html" ]]
-    then 
-        continue    
-    fi
-    ESCAPED_KEYWORD=$(printf '%s\n' "$source" | sed -e 's/[]\/$*.^[]/\\&/g');
-    ESCAPED_REPLACE=$(printf '%s\n' "$data" | sed -e 's/[\/&]/\\&/g');
-    filePath=$(echo $f | sed "s/$ESCAPED_KEYWORD/$ESCAPED_REPLACE/g")
-    mkdir -p $(dirname "$filePath")
-    echo "saving commits into $filePath.json"
-    git log --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%s"%n, },' --follow $f | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/' > $filePath.json
-  fi
-done
+cd $(readlink -f "$1")
+file=$(readlink -f "$2")
+if [[ -f $file ]]
+then
+  git log --date=short --pretty=format:'{%n  "sha": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%s",%n  "email": "%aE",%n  "name": "%aN"%n },' --follow $file | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/'
+else
+  echo "not a file: $file"
+  exit 1
+fi
 }
 
-gitlog $1 $2 $3
+gitlog $1 $2
