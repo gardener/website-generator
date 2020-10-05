9d268aa15ce7c2a9b7c44b50722fb520c69625e5
Svilen Ivanov <svilen.ivanov@sap.com>
2020-10-05 16:57:12 +0300

    make clone_doc_version.sh of BUILD env var

--- .ci/clone_doc_version.sh
+++ .ci/clone_doc_version.sh
@@ -15,15 +15,24 @@ npm prune --production
 CLONE="temp"
 echo "Cloning from ${FORK}"
 git clone "https://github.com/${FORK}/documentation.git" "$CLONE"
-if [ "$BUILDSINGLEBRANCH" = "true" ]; then
-  echo "Building site from documentation version ${BRANCH}"
-  (cd $CLONE && git checkout "origin/$BRANCH" -b ${BRANCH})
+dir="${CLONE}/website/*"
+cp -r $dir hugo/content
+
+# In case of head update we just have to copy blogs and adopters from the master
+if [[ -z $BUILD || $BUILD == "head-update" ]]; then
+  exit 0
+fi
+
+if [[ "$BUILDSINGLEBRANCH" = "true" ]]; then
+  # Switch to the desired branch.
+  if [[ $BRANCH != "master" ]]; then
+    echo "Building site from documentation version ${BRANCH}"
+    (cd $CLONE && git checkout "origin/$BRANCH" -b ${BRANCH})
+  fi
+
   dir="${CLONE}/website/*"
   echo "docforge -f "${CLONE}/doc.yaml" -d hugo/content/ --hugo --github-oauth-token $GIT_OAUTH_TOKEN --markdownfmt=true"
   docforge -f "${CLONE}/doc.yaml" -d hugo/content/ --hugo --github-oauth-token $GIT_OAUTH_TOKEN --markdownfmt=true
-  # with single branch we can directly move the contents of the repo inside
-  # hugo/content because we don't use the repo later.
-  mv $dir hugo/content
   export CONTENT="$CLONE/website/"
   export DATA="hugo/data/"
   echo "Calling nojeds script with \$DATA=$DATA and CONTENT=$CONTENT"
@@ -32,17 +41,20 @@ else
   echo "Building site from the last ${n} documentation versions"
   while [[ $r -le $LATESTVERSION ]]; do
     echo 'Getting docs from: v1.'"${r}"'.0'
+    # Switch to desired release version
     version="v1.${r}.0"
     (cd $CLONE && git checkout "tags/${version}" -b ${version})
+
     export CONTENT="$CLONE/website/documentation"
     export DATA="hugo/data/v1.${r}.0"
+
+    # In case the latest documentation version set as root.
     if [ "$r" = "$LATESTVERSION" ]; then
-      dir="${CLONE}/website/*"
-      cp -r $dir hugo/content
       version="documentation"
       export CONTENT="$CLONE/website/"
       export DATA="hugo/data/"
     fi
+
     echo "docforge -f "${CLONE}/doc.yaml" -d "hugo/content/${version}" --hugo --github-oauth-token $GIT_OAUTH_TOKEN --markdownfmt=true"
     docforge -f "${CLONE}/doc.yaml" -d "hugo/content/${version}" --hugo --github-oauth-token $GIT_OAUTH_TOKEN --markdownfmt=true
     echo "Calling nojeds script with \$DATA=$DATA and CONTENT=$CONTENT"
