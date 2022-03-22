d94beb846751b0e77a113bf0b1f0009c698327ef
Dimitar Kostadinov <dimitar.kostadinov@gmail.com>
2022-03-22 13:44:04 +0200

    Update npm dependencies and fix shellcheck issues

--- scripts/mklink.sh
+++ scripts/mklink.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cmd.exe /C "mklink /D "${1//\//\\}" "${2//\//\\}
+cmd.exe /C "mklink" /D "${1//\//\\}" "${2//\//\\}"
 
 # Alias: mklink=" . mklink.sh"
 # USAGE: mklink <mylink> <target>
