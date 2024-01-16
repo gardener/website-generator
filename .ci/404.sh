8d92b8fa1e3cedd81b88ab5f3633719fc41eeca7
Viktor <mail.victor.kostov@gmail.com>
2024-01-16 17:35:19 +0200

    404.sh and html page

---
+++ .ci/404.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+pushd hugo/content || exit
+
+cat > ../static/js/404.js <<EOF
+links = [
+EOF
+
+find . -type f \
+    | grep -v "__resources" \
+    | sed -E 's|\.md$|/",|; s|_index/||; s|^\.|"https://gardener.cloud|; ' >> ../static/js/404.js
+
+cat >> ../static/js/404.js <<EOF
+]
+
+titles = [
+EOF
+
+find . -type f -exec grep -H '^title: ' {} \; | sed 's/.*title: //; s/"//g; s/^/"/; s/$/",/'   >> ../static/js/404.js
+
+cat >> ../static/js/404.js <<EOF
+]
+
+let ul = document.getElementById('links');
+
+links.map((element, index) => [element, titles[index]]
+).filter( el => {
+    let splitUrl = window.location.href.split("/");
+    return el[0].endsWith("/" + splitUrl[splitUrl.length - 2] + "/")
+}).map( el => {
+    let li = document.createElement('li');
+    let a = document.createElement('a')
+    a.textContent = el[1]
+    a.setAttribute("href",el[0])
+    li.appendChild(a)
+    ul.appendChild(li);
+})
+
+EOF
+
+popd || exit
